90f5ad00bf25be66be4b87bd84154ff55d15b379
mey <meydetour@gmail.com>
2023-10-07 18:29:04 +0200

    j

---
+++ script.js
@@ -0,0 +1,14 @@
+<div className="description d-block">
+    h2>A Girl & HER Guard Dog</h2>
+        <h6>Genre : Shojo, Comédie, Romance, School Life</h6>
+
+        <h6>Nombre d’épisodes : 13</h6>
+        <p><Isaku Senagaki, redoutée en raison du clan Yakuza de son grand-père, entre au lycée pour vivre normalement et
+        trouver l'amour. Keiya Utou, jeune chef du clan, falsifie son identité pour la protéger, ignorant qu'elle l'aime
+        en secret. </p>
+        <br><br><br><a href="" className="fs-4 bg-white rounded-pill p-3 text-decoration-none text-violet ">Aller à la
+            page <img src="icone/teteChinois.png" width="50px" height="50px"></a>
+
+
+</div>
+
